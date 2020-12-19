8043cb32c684fdaa08ad931a943ad854677ee907
Edgar Zea <edgarzea@MacBook-Pro-de-Edgar.local>
2020-12-19 00:36:08 -0600

    Native JS API

--- 34-APIs-JS/js/01-app.js
+++ 34-APIs-JS/js/01-app.js
@@ -91,3 +91,14 @@ document.addEventListener('visibilitychange', () => {
 // };
 
 // micro.onclick = requestPermission;
+
+// Intersection Observer
+
+window.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver(entries => {
+    console.log(entries);
+    console.log(entries[0]);
+  });
+
+  observer.observe(document.querySelector('.premium'));
+});
--- 34-APIs-JS/js/02-app.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver(entries => {
-    console.log(entries);
-    console.log(entries[0]);
-  });
-
-  observer.observe(document.querySelector('.premium'));
-});
