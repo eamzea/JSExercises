c83edd18a855f1fc6c315915acb27f8ab3ac967b
Edgar Zea <cesszea@icloud.com>
2021-02-08 16:17:02 -0600

    Webpack, Eslint and Babel added

---
+++ 53-Webpack-ESLint/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '/'),
+    compress: true,
+    port: 5000,
+    publicPath: '/public/js/',
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules), (entrada.js), (salida.js)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /(node_modules), (entrada.js), (salida.js)/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+    ],
+  },
+};
